a9240062d9228c3766f70ed164724afb4bfd2087
diegobajetti <diego.bajetti@outlook.com>
2023-07-06 19:43:21 -0400

    Updates

---
+++ precache-manifest.cbc44416ddea01ba9f904e33d4c1296c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd0dac00b6438573330e",
+    "url": "/check-my-attendance/static/css/main.aca003ca.chunk.css"
+  },
+  {
+    "revision": "fd0dac00b6438573330e",
+    "url": "/check-my-attendance/static/js/main.046d0dba.chunk.js"
+  },
+  {
+    "revision": "1efeef4a657d1316662c",
+    "url": "/check-my-attendance/static/js/runtime~main.aa80820c.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/check-my-attendance/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/check-my-attendance/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/check-my-attendance/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7867a721d9ae7da0ee0e7847d6633484",
+    "url": "/check-my-attendance/index.html"
+  }
+];
